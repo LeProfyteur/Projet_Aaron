0413d316affd5bd32dda396e8b010421efb40b4e
Vincent <vincent.givaudan@hotmail.fr>
2020-02-18 14:47:24 +0100

    Equipement de base + interface

---
+++ Source/Projet_Aaron/Equipment/EquipmentInterface.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EquipmentInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UEquipmentInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJET_AARON_API IEquipmentInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void Activate();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void OnEquip();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void OnUnequip();
+};
