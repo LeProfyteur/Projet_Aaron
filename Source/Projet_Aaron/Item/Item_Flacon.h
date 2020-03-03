281f872e31ec311d25066107b3c98de15dfc80e2
Lupusanghrenn <brisingr34130@gmail.com>
2020-03-03 14:24:45 +0100

    Creating FlaconCPP / Rename folder syringe

---
+++ Source/Projet_Aaron/Item/Item_Flacon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Item_Flacon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_AARON_API AItem_Flacon : public AItem
+{
+	GENERATED_BODY()
+	
+};
