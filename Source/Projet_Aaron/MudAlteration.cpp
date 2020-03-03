70e85cb73e248809cb0d8f021b88f8fd03d41bd8
Thomas <wapps3@gmail.com>
2020-03-03 18:03:48 +0100

    Add MudAlteration and update Mud with new version

---
+++ Source/Projet_Aaron/MudAlteration.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MudAlteration.h"
+#include "Perception/AIPerceptionStimuliSourceComponent.h"
+#include "AISense_Smell.h"
+
+UMudAlteration::UMudAlteration()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	bAutoActivate = true;
+	bAutoRegister = true;
+}
+
+void UMudAlteration::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetOwner()->FindComponentByClass<UAIPerceptionStimuliSourceComponent>()->UnregisterFromSense(UAISense_Smell::StaticClass());
+}
+
+void UMudAlteration::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}
+
+void UMudAlteration::OnComponentDestroyed(bool bDestroyingHierarchy)
+{
+	UE_LOG(LogTemp, Warning, TEXT("je meurs"));
+}
+
+
+
+
+
+
+
+
