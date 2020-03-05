d408966992a4b1d6101109c9800b44e19a7effbd
Thomas <wapps3@gmail.com>
2020-03-05 17:24:43 +0100

    Add AdrenalineAlteration and Update FlaconAdreline to work with it

---
+++ Source/Projet_Aaron/AdrenalineAlteration.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ShapeAlteration.h"
+#include "Projet_Aaron/StatManager/CreatureStatManager.h"
+
+#include "AdrenalineAlteration.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PROJET_AARON_API UAdrenalineAlteration : public UShapeAlteration
+{
+	GENERATED_BODY()
+
+public :
+
+	UAdrenalineAlteration();
+	
+	virtual void BeginPlay() override;
+
+	virtual void OnComponentDestroyed(bool bDestroyingHierarchy) override;
+	
+};
