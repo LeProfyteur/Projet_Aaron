cf95cdcee8186e31b2968c92d4640457b9109df9
Lupusanghrenn <brisingr34130@gmail.com>
2020-03-10 17:22:47 +0100

    Create Enum and DataTable for Equipment

---
+++ Source/Projet_Aaron/EnumEquipement.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum EnumEquipment
+{
+	EE_Head UMETA(DisplayName = "Head"),
+	EE_Torso UMETA(DisplayName = "Torso"),
+	EE_Arm UMETA(DisplayName = "Arm"),
+	EE_Leg UMETA(DisplayName = "Legs"),
+	EE_Abilities UMETA(DisplayName = "Abilities"),
+	EE_Abilities2 UMETA(DisplayName = "Abilities"),
+};
+
