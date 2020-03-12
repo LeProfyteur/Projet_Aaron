ac560afbfae0afc77fa79db17415a80e5a14258c
Thomas <wapps3@gmail.com>
2020-03-12 16:19:29 +0100

    Create subsystem to handle dialog

---
+++ Source/Projet_Aaron/Dialog/DialogSubsystem.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DialogSubsystem.h"
+#include "Engine/Engine.h"
+
+void UDialogSubsystem::SetMetric(FString MetricToUpdate, int32 NewValue)
+{
+	if(GEngine)
+	{
+		if( UDialogSubsystem* DialogSubsystem = GEngine->GetEngineSubsystem<UDialogSubsystem>() )
+		{
+			DialogSubsystem->Metrics.Add(MetricToUpdate, NewValue);
+		}
+	}
+}
+
+void UDialogSubsystem::IncrementMetric(FString MetricToUpdate)
+{
+	if (GEngine)
+	{
+		if (UDialogSubsystem* DialogSubsystem = GEngine->GetEngineSubsystem<UDialogSubsystem>())
+		{
+			DialogSubsystem->Metrics.FindOrAdd(MetricToUpdate)++;
+		}
+	}
+}
+
+void UDialogSubsystem::UpdateKnowledge(FString KnowledgeToUpdate)
+{
+	if (GEngine)
+	{
+		if (UDialogSubsystem* DialogSubsystem = GEngine->GetEngineSubsystem<UDialogSubsystem>())
+		{
+			DialogSubsystem->Knowledge.Add(KnowledgeToUpdate, true);
+		}
+	}
+}
+
+int UDialogSubsystem::GetMetric(FString MetricToGet)
+{
+	if (GEngine)
+	{
+		if (UDialogSubsystem* DialogSubsystem = GEngine->GetEngineSubsystem<UDialogSubsystem>())
+		{
+			if(int32* ptr = DialogSubsystem->Metrics.Find(MetricToGet))
+			{
+				return *ptr;
+			}
+		}
+	}
+	return 0;
+}
+
+bool UDialogSubsystem::GetKnowledge(FString KnowledgeToGet)
+{
+	if (GEngine)
+	{
+		if (UDialogSubsystem* DialogSubsystem = GEngine->GetEngineSubsystem<UDialogSubsystem>())
+		{
+			if (bool* ptr = DialogSubsystem->Knowledge.Find(KnowledgeToGet))
+			{
+				return *ptr;
+			}
+		}
+	}
+	return false;
+}
---
+++ Source/Projet_Aaron/Dialog/DialogSubsystem.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "DialogSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_AARON_API UDialogSubsystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "metrics")
+		TMap<FString, int32> Metrics;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "metrics")
+		TMap<FString, bool> Knowledge;
+
+	UFUNCTION(BlueprintCallable)
+		static void SetMetric(FString MetricToUpdate, int32 NewValue);
+
+	UFUNCTION(BlueprintCallable)
+		static void IncrementMetric(FString MetricToUpdate);
+
+	UFUNCTION(BlueprintCallable)
+		static void UpdateKnowledge(FString KnowledgeToUpdate);
+
+	UFUNCTION(BlueprintCallable)
+		static int GetMetric(FString MetricToGet);
+
+	UFUNCTION(BlueprintCallable)
+		static bool GetKnowledge(FString KnowledgeToGet);
+};
